b23e9f3c751e51f57b69905196e7a82cb417bc31
TamannaAfroz2666 <afroztamanna66@gmail.com>
2024-02-17 01:10:48 +0600

    about page description done

--- src/Layout/Main.js
+++ src/Layout/Main.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import './Main.css';
 import Header from './Header/Header';
+import About from '../components/About/About';
 
 const Main = () => {
     return (
         <div className='mainContainer'>
             <div className="headerMain">
                 <Header></Header>
+                <div className="aboutSec">
+                    <About></About>
+                </div>
             </div>
             
         </div>
---
+++ src/components/About/About.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './About.css';
+import sing from '../../assets/Images/About/sing22.jpg'
+
+const About = () => {
+    return (
+        <div className='aboutContainer'>
+            <div className="aboutSubContainer">
+                <h5 className='titleAbout'>about us</h5>
+                <p className='desAbout'>Divide have don't man wherein air fourth.Own itself make have night won't make. <br></br> A you under Seed appear which good give.Own give air without fowl moveth dry first heaven fruit, dominion she'd won't very all</p>
+                <div className="singContainer">
+                    <img src={sing} alt="" />
+                </div>
+
+
+            </div>
+
+        </div>
+    );
+};
+
+export default About;
