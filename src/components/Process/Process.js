54ba06c3fd2ce9f14e5459dfa0475a2010ace37a
TamannaAfroz2666 <afroztamanna66@gmail.com>
2024-02-17 15:56:04 +0600

    process section done

--- src/components/Process/Process.js
+++ src/components/Process/Process.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import './Process.css';
+import { FaPlayCircle } from "react-icons/fa";
+// import { FaPlayCircle } from "react-icons/fa";
+import { MdPlayCircle } from "react-icons/md";
+import { FaRegPlayCircle } from "react-icons/fa";
+
 
 const Process = () => {
     return (
@@ -7,6 +12,14 @@ const Process = () => {
             <div className="processSec">
                 <h5 className='titleProcess'>our work process</h5>
                 <p className='desAbout'>Was years its sessions was there from he in them together over that, <br></br> third sixth gathered female creeping bearing behold years</p>
+            </div>
+            <div className="processImg">
+                <div className="imgSection">
+                    <div className="watchVideoLink">
+                        <a className='watchLink' href="/watch-video">  <MdPlayCircle size={80} className='faplayCir' /> </a>
+                    </div>
+                    {/* <FaPlayCircle  size={40}/> */}
+                </div>
             </div>
 
         </div>
