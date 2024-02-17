4df479de3cd693e020d8e304c23c2b6a792daa9f
TamannaAfroz2666 <afroztamanna66@gmail.com>
2024-02-17 19:33:59 +0600

    contact page done

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -5,12 +5,32 @@ const Contact = () => {
     return (
         <div className='contactContainer'>
             <div className="contactSections">
-            <h5 className='titleContact'>Need a Project ?</h5>
+                <h5 className='titleContact'>Need a Project ?</h5>
                 <p className='desAbout'>Let us know what you're looking for in an agency. We'll take a look and see.  <br></br> If this could be the start of something beautiful.</p>
+            </div>
+            <div className="contactContents">
+                <div className="nameEmailSec">
+                    <div className="nameSection">
+                        <input type="text" name='name' className='nameInputField' placeholder='Your Name' autoComplete='off' />
+                    </div>
+                    <div className="nameSection">
+                        <input type="email" name='name' className='nameInputField' placeholder='Your Email' autoComplete='off' />
+                    </div>
+                </div>
+                <div className="nameSection">
+                    <input type="text" name='title' className='titleInputField' placeholder='Your Title' autoComplete='off' />
+                </div>
+                <div className="nameSection">
+                    <input type="text" name='comment' className='commentInputField' placeholder='Your Comment' autoComplete='off' />
+                </div>
+                <div className="sendBtn">
+                    <button>send message</button>
+                </div>
+
 
             </div>
-         
-            
+
+
         </div>
     );
 };
