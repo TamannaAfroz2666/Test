4e7f4204d13e282bbdfffa76b336f33e1e34a9ae
TamannaAfroz2666 <afroztamanna66@gmail.com>
2024-02-17 08:23:05 +0600

    about page done

--- src/page/AboutPage.js
+++ src/page/AboutPage.js
@@ -13,10 +13,36 @@ const AboutPage = () => {
                             <h5 className='titles' >75%</h5>
                         </div>
                         <div className="progressSec">
-                        <progress className='progress-bar' id="file" max="100" value="75"></progress>
+                            <progress className='progress-bar' id="file" max="100" value="75"></progress>
                         </div>
-                       
                     </div>
+                    <div className="uiDesignSec">
+                        <div className="skillTitleCon">
+                            <div className="titlesSection">
+                                <h5 className='titles'>web development</h5>
+                                <h5 className='titles' >90%</h5>
+                            </div>
+                            <div className="progressSec">
+                                <progress className='progress-bar' id="file" max="100" value="90"></progress>
+                            </div>
+
+                        </div>
+
+                    </div>
+                    <div className="uiDesignSec">
+                        <div className="skillTitleCon">
+                            <div className="titlesSection">
+                                <h5 className='titles'>marketing</h5>
+                                <h5 className='titles' >65%</h5>
+                            </div>
+                            <div className="progressSec">
+                                <progress className='progress-bar' id="file" max="100" value="65"></progress>
+                            </div>
+
+                        </div>
+
+                    </div>
+
 
                 </div>
 
